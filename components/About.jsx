b3a5df3157d2d6b1b8e415f13503e8e38972dd90
Irshad k <irshadkmpm656@gmail.com>
2025-02-19 17:54:48 +0530

    completed

--- components/About.jsx
+++ components/About.jsx
@@ -1,40 +1,70 @@
 import { assets, infoList, toolsData } from "@/assets/assets";
 import Image from "next/image";
 import React from "react";
+import {motion} from "motion/react"
 
-const About = () => {
+const About = ({isDarkMode}) => {
   return (
-    <div id="about" className="w-full px-[12%] py-10 scroll-mt-20">
-      <h4 className="text-center mb-2 text-lg font-Ovo"> Introduction</h4>
-      <h2 className="text-center text-5xl font-Ovo">About me</h2>
-      <div className="flex w-full flex-col lg:flex-row items-center gap-20 my-20">
-        <div className="w-64 sm:w-80 rounded-3xl max-w-none">
+    <motion.div id="about" className="w-full px-[12%] py-10 scroll-mt-20"
+    initial={{opacity:0}}
+    whileInView={{opacity:1}}
+    transition={{duration:1}}
+    >
+      <motion.h4 className="text-center mb-2 text-lg font-Ovo"
+      initial={{opacity:0,y:-20}}
+      whileInView={{opacity:1,y:0}}
+      transition={{duration:0.5,delay:0.3}}
+      > Introduction</motion.h4>
+      <motion.h2 className="text-center text-5xl font-Ovo"
+      initial={{opacity:0,y:-20}}
+      whileInView={{opacity:1,y:0}}
+      transition={{duration:0.5,delay:0.5}}
+      >About me</motion.h2>
+      <motion.div className="flex w-full flex-col lg:flex-row items-center gap-20 my-20"
+      initial={{opacity:0}}
+      whileInView={{opacity:1}}
+      transition={{duration:0.8}}
+      >
+        <motion.div className="w-64 sm:w-80 rounded-3xl max-w-none"
+        initial={{opacity:0,scale:0.9}}
+        whileInView={{opacity:1,scale:1}}
+        transition={{duration:0.6}}
+        >
           <Image
             src={assets.user_image}
             alt="user"
             className="w-full rounded-3xl"
           />
-        </div>
-        <div className="flex-1">
+        </motion.div>
+        <motion.div className="flex-1"
+            initial={{opacity:0}}
+            whileInView={{opacity:1}}
+            transition={{duration:0.6,delay:0.8}}
+        >
           <p className="mb-10 max-w-2xl font-Ovo">
             I am a Frontend Developer with 2 years of experience specializing in
             building responsive and dynamic web applications. Proficient in
             React.js and Next.js, I focus on creating seamless user experiences
             with clean and efficient code.
           </p>
-          <ul className="grid grid-cols-1 sm:grid-cols-3 gap-6">
+          <motion.ul className="grid grid-cols-1 sm:grid-cols-3 gap-6"
+              initial={{opacity:0}}
+              whileInView={{opacity:1}}
+              transition={{duration:0.8,delay:1}}
+          >
             {infoList.map(({ icon, iconDark, title, description }, index) => (
-              <li
-                className="border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-black"
+              <motion.li
+              whileInView={{scale:1.05}}
+                className="border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-black dark:border-white dark:hover:shadow-white dark:hover:bg-darkHover/50"
                 key={index}
               >
-                <Image className="w-7 mt-3" src={icon} alt={title} />
-                <h3 className="my-4 font-semibold text-gray-700">{title}</h3>
-                <p className="text-gray-600 text-sm">{description}</p>
-              </li>
+                <Image className="w-7 mt-3" src={isDarkMode ?iconDark :icon} alt={title} />
+                <h3 className="my-4 font-semibold text-gray-700 dark:text-white">{title}</h3>
+                <p className="text-gray-600 text-sm dark:text-white/80">{description}</p>
+              </motion.li>
             ))}
-          </ul>
-          <h4 className="my-6 text-gray-700 font-Ovo">Tools I use</h4>
+          </motion.ul>
+          <h4 className="my-6 text-gray-700 font-Ovo dark:text-white/80">Tools I use</h4>
           <ul className="flex items-center gap-3 sm:gap-5">
             {toolsData.map((tool, index) => (
               <li
@@ -45,9 +75,9 @@ const About = () => {
               </li>
             ))}
           </ul>
-        </div>
-      </div>
-    </div>
+        </motion.div>
+      </motion.div>
+    </motion.div>
   );
 };
 
